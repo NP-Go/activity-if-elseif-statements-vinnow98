16391047623c9ea689ef21acb11da51db11051f5
vinnow98 <evinwoon@gmail.com>
2022-03-30 09:25:28 +0800

    Added activity 3

--- ifElseIfStatements.go
+++ ifElseIfStatements.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main() {
+func activity2() {
 	number1 := 17
 	resultMessage := "No outcome."
 	if number1%2 == 0 {
@@ -20,4 +20,21 @@ func main() {
 	} else {
 		fmt.Println("This number has only 1 digit")
 	}
+}
+func activity3() {
+	var input int
+	fmt.Println("Input a number!")
+	fmt.Scanln(&input)
+	a := input%5 == 0
+	b := input%6 == 0
+	if a && b {
+		fmt.Println("This number is divisible by 5 and 6")
+	} else {
+		fmt.Println("This number is not divisible by 5 and 6")
+	}
+
+}
+func main() {
+	activity2()
+	activity3()
 }
