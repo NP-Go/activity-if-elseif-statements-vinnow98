29d35e9617006f9471cd78f572d3d9130ad6583a
vinnow98 <evinwoon@gmail.com>
2022-04-01 06:58:12 +0800

    Completed! Shited activity 3 away

--- ifElseIfStatements.go
+++ ifElseIfStatements.go
@@ -21,20 +21,7 @@ func activity2() {
 		fmt.Println("This number has only 1 digit")
 	}
 }
-func activity3() {
-	var input int
-	fmt.Println("Input a number!")
-	fmt.Scanln(&input)
-	a := input%5 == 0
-	b := input%6 == 0
-	if a && b {
-		fmt.Println("This number is divisible by 5 and 6")
-	} else {
-		fmt.Println("This number is not divisible by 5 and 6")
-	}
 
-}
 func main() {
 	activity2()
-	activity3()
 }
